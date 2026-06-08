bf165434d220a981f3c8c416ac5ad40363f6b746
김시연 <siyeon20040311@gmail.com>
2026-06-08 13:21:24 +0900

    Add Agg backend #1

--- graph.py
+++ graph.py
@@ -1,5 +1,9 @@
 from urllib.parse  import parse_qs
 from template import html
+
+import matplotlib as mpl
+mpl.use('Agg')
+
 import matplotlib.pyplot as plt
 
 def application(environ, start_response):
